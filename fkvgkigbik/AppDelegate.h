7442025d5909f5196da3b99df5a0706b030ce415
AMIT <amit@amits-macbook-pro.local>
2018-07-12 16:58:37 +0200

    Initial Commit

---
+++ fkvgkigbik/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fkvgkigbik
+//
+//  Created by AMIT on 7/12/18.
+//  Copyright © 2018 com.hgdhghdgfd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ fkvgkigbik/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  fkvgkigbik
+//
+//  Created by AMIT on 7/12/18.
+//  Copyright © 2018 com.hgdhghdgfd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
